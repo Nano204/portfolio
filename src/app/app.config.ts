f8491bd8060eb301b77e7b4e3e7023efb835a726
Nano204 <fgomeztenorio@hotmail.com>
2024-05-25 11:41:38 -0500

    NDP(004): Added navbar.
    Added fonts.
    
    Added custom styles.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  NgxBootstrapIconsModule,
+  house,
+  journalCode,
+  personCircle,
+  stack,
+} from 'ngx-bootstrap-icons';
 
 import { routes } from './app.routes';
 
+// TODO: Figure out if there is any way to provide the icon list directly in the component
+// so it's not necessary to import them for all the app
+const icons = {
+  house,
+  personCircle,
+  journalCode,
+  stack,
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      NgxBootstrapIconsModule.pick(icons, { width: '1.7rem', height: '1.7rem' }),
+    ),
+  ],
 };
