b04062947a83e03156a89d3211d35496058da7d2
Nano204 <fgomeztenorio@hotmail.com>
2024-05-27 20:58:03 -0500

    NDP(007): Added profile details section base.

---
+++ src/app/profile/profile-details/profile-details.script.ts
@@ -0,0 +1,71 @@
+import { AcademicDetails } from './academic-details/models';
+import { ExperienceDetails, Technologies } from './experience-details/models';
+
+export const experienceDetails: ExperienceDetails[] = [
+  {
+    startDate: 1716856178,
+    finalDate: 0,
+    role: 'Software Developer',
+    company: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+    stack: [
+      Technologies.TYPESCRIPT,
+      Technologies.ANGULAR,
+      Technologies.EXPRESS,
+    ],
+  },
+  {
+    startDate: 1716856178,
+    finalDate: 1716853424,
+    role: 'Software Developer',
+    company: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+    stack: [
+      Technologies.TYPESCRIPT,
+      Technologies.ANGULAR,
+      Technologies.EXPRESS,
+    ],
+  },
+  {
+    startDate: 1716856178,
+    finalDate: 1716853424,
+    role: 'Software Developer',
+    company: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+    stack: [
+      Technologies.TYPESCRIPT,
+      Technologies.ANGULAR,
+      Technologies.EXPRESS,
+    ],
+  },
+];
+
+export const academicDetails: AcademicDetails[] = [
+  {
+    startDate: 1716856178,
+    finalDate: 0,
+    course: 'Software Developer',
+    institution: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+  },
+  {
+    startDate: 1716856178,
+    finalDate: 1716853424,
+    course: 'Software Developer',
+    institution: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+  },
+  {
+    startDate: 1716856178,
+    finalDate: 1716853424,
+    course: 'Software Developer',
+    institution: 'Jalasoft',
+    description:
+      'Full-stack developer in an application for process management, featuring interactive workflows for tracking and control. Specialized in designing and creating comprehensive technological solutions, encompassing both frontend and backend development. Focused on implementing a well-structured software architecture, emphasizing modularity, readability, and best coding practices to ensure system efficiency, maintainability, and scalability.',
+  },
+];
